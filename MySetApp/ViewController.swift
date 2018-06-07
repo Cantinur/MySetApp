1f83bb50584cda564a8fa14d5328706e61530ea1
Henrik Sandberg <henrikaos91@gmail.com>
2018-06-07 14:59:34 +0200

    Initial Commit

---
+++ MySetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySetApp
+//
+//  Created by Henrik Anthony Odden Sandberg on 6/7/18.
+//  Copyright © 2018 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
